accb76c67b93121cc98ae0cc38a207fe60772926
Pavel Lebedev <lebedpavel.dev@gmail.com>
2022-05-02 18:58:24 +0300

    feat: end 3 task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let dns = {};
+  let curDns = '';
+  for (let i = 0; i < domains.length; i++) {
+    let arrDns = domains[i].split('.').reverse();
+    for (let j = 0; j < arrDns.length; j++) {
+      curDns += `.${arrDns[j]}`;
+      if (!dns[curDns]) {
+        dns[curDns] = 1;
+      } else {
+        dns[curDns]++
+      }
+    }
+    curDns = '';
+  }
+  return dns;
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  console.log(str);
+  let newLine = '';
+  let repeatChar;
+  let numRepeat = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      repeatChar = str[i];
+      numRepeat++;
+    } else {
+      if (numRepeat > 1) {
+        newLine += `${numRepeat}${repeatChar}`;
+        repeatChar = '';
+        numRepeat = 1;
+      } else {
+        newLine += str[i];
+      }
+    }
+  }
+  return newLine;
 }
 
 module.exports = {
